c5d540f9b49f55a4036b82f20481d7f58dc39682
MHSSWWRR <sularsofadhila@gmail.com>
2023-11-28 22:47:01 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,17 +1,11 @@
 <?php
 $server = "localhost";
-$username = "root";
-$password = "";
-$database = "tangledd";
+$username = "ecom4781_FadhilaGalih";
+$password = "2300227756Adek.";
+$database = "ecom4781_rapunzel";
 
 // Membuat koneksi ke database
 $conn = mysqli_connect($server, $username, $password, $database);
 
-// Memeriksa koneksi
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
-
 mysqli_set_charset($conn, "utf8");
-
 ?>
