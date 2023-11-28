512ae05abb78701576195c8bb12e9edb32b663b9
MHSSWWRR <sularsofadhila@gmail.com>
2023-11-28 08:20:29 +0700

    Membuat halaman login
    Rancangan awal login

--- login/login.php
+++ login/login.php
@@ -1,15 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="login.css">
-    <title>login</title>
-</head>
-<body>
-    <div class='login'>
-pppppppppppppppp
-    </div>
-    
-</body>
-</html>
+<?php
+session_start();
+require_once('koneksi.php');
+
+// Koneksi ke Database
+$conn = new mysqli("localhost", "root", "", "tangledd");
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Ambil data dari form
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Query untuk memeriksa akun
+$sql = "SELECT * FROM account WHERE username = '$username' AND password = '$password'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Akun ditemukan
+    $row = $result->fetch_assoc();
+    $role = $row['role'];
+
+    // Redirect sesuai peran
+    if ($role === 'storyteller') {
+        header("Location: admin_dashboard.php"); // Ganti dengan URL halaman admin
+    } else if ($role === 'reader') {
+        echo "Login successful. Welcome, $username!";
+        // Redirect ke halaman pengguna biasa 
+    }
+} else {
+    echo "Login failed. Invalid username or password.";
+}
+
+$conn->close();
+?>
+
